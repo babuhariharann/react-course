5cdd832edce96563bf4c1b4b5df91115bf7008b6
babuhariharann <babuhariharann@gmail.com>
2026-01-10 19:46:39 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1", { id: "heading" }, "Hariharan");
+const heading2 = React.createElement("h2", { id: "heading2" }, "Heading 2 tag");
+const child = React.createElement("div", { id: "child" }, [heading, heading2]);
+const parent = React.createElement("div", { id: "parent" }, child);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ README.md
@@ -0,0 +1,1 @@
+# React course - Episode 01
