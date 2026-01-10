4ca047275fb24e1213caff3d6eed16c3565fa34d
babuhariharann <babuhariharann@gmail.com>
2026-01-10 19:49:27 +0530

    Episode-01-changes

--- App.js
+++ App.js
@@ -3,4 +3,5 @@ const heading2 = React.createElement("h2", { id: "heading2" }, "Heading 2 tag");
 const child = React.createElement("div", { id: "child" }, [heading, heading2]);
 const parent = React.createElement("div", { id: "parent" }, child);
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(parent);
